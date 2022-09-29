5d475caba871663302315ae711b91e470c41cd5b
ThareqFatih <112853686+ThareqFatih@users.noreply.github.com>
2022-09-29 18:56:54 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        nazie: "#0ea5e9",
+        dark: " #1e293b",
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
